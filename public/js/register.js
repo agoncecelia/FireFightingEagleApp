bca5ccdff9adeb2d097a4fc0f5e25fc57b266950
Semi Nebiu <semi.nebiu@bridge-technology.net>
2017-05-09 16:13:47 +0200

    register

--- public/js/register.js
+++ public/js/register.js
@@ -1,19 +1,88 @@
 var map;
 var marker = null;
+var drawingManager;
+var polygon;
+
+var register = {
+    departmentName: null,
+    servingArea: {
+        type: 'Polygon',
+        coordinates: null,
+    },
+    deparmentLocation: {
+        coordinates: null
+    },
+    email: null,
+    username: null,
+    password: null
+}
+
+$('body').on('keyup', '.inputs', function () {
+    $(this).css('border-color', '#ccc');
+});
 
 $('body').on('click', '#btnNext', function () {
-    $('.register-container').hide();
-    $('.register-map').show();
+    if(validateInputs('inputs')) {
+        if($("input[name=password").val() !== $("input[name=retypePassword").val()){
+            alert('Password did not match!');
+            $("input[name=retypePassword").css('border-color', 'red');
+            return;
+        }
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 16.460134, lng: -12.488326},
-        zoom: 2,
-        minZoom: 2,
-        disableDefaultUI: true,
-    });
+        $('.register-container').hide();
+        $('.register-map').show();
 
-    autoComplete();
+        register.departmentName = $("input[name=name").val();
+
+        register.email = $("input[name=email").val();
+
+        register.username = $("input[name=username").val();
+
+        register.password = $("input[name=password").val();
+
+
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 16.460134, lng: -12.488326},
+            zoom: 2,
+            minZoom: 2,
+            disableDefaultUI: true,
+        });
+
+        autoComplete();
+    }
 });
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function validateInputs(inputClass) {
+    var inputs = $('.' + inputClass);
+    var isInvalid = false;
+
+    for(var i = 0; i < inputs.length; i++){
+        if($(inputs[i]).val() == ''){
+            isInvalid = true;
+
+            $(inputs[i]).css('border-color', 'red');
+        }
+
+        if($(inputs[i]).attr('type') == 'email'){
+            if(!validateEmail($(inputs[i]).val())){
+                isInvalid = true;
+
+                $(inputs[i]).css('border-color', 'red');
+            }
+        }
+    }
+
+    if(isInvalid){
+        return false
+    }else{
+        return true;
+    }
+}
 
 $('body').on('click', '#btnNextPolygon', function () {
     clearMap();
@@ -29,10 +98,21 @@ function clearMap() {
     $("#btnNextPolygon").remove();
     $("#pac-input").remove();
 }
+var enabledPolygon = {
+    drawingControlOptions: {
+        position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: ['polygon']
+    }
+}
+var disabledPolygon = {
+    drawingControlOptions:{
+        position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: []
+    }
+}
 
 function initPolygon() {
-    var drawingManager = new google.maps.drawing.DrawingManager({
-        drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingManager = new google.maps.drawing.DrawingManager({
         drawingControl: true,
         drawingControlOptions: {
             position: google.maps.ControlPosition.TOP_CENTER,
@@ -40,10 +120,50 @@ function initPolygon() {
         },
         polygonOptions: {
             draggable: true,
+            editable: true
         }
     });
 
     drawingManager.setMap(map);
+
+    google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
+        polygon = event;
+
+        convertCoords(event.overlay);
+
+        var button = '<button class="btn" id="btnFinishPolygon">Finish</button>';
+
+        $('.register-map').append(button)
+
+        drawingManager.setOptions(disabledPolygon);
+
+        google.maps.event.addListener(polygon.overlay.getPath(), 'insert_at', function() {
+            convertCoords(polygon.overlay)
+        });
+
+        google.maps.event.addListener(polygon.overlay.getPath(), 'remove_at', function() {
+            convertCoords(polygon.overlay)
+        });
+
+        google.maps.event.addListener(polygon.overlay.getPath(), 'set_at', function() {
+            convertCoords(polygon.overlay)
+        });
+    });
+}
+
+function convertCoords(overlay) {
+    var positions = overlay.getPath().getArray();
+    var coords = [];
+
+    for(var i = 0; i < positions.length; i++){
+        var row = positions[i];
+        coords.push({
+            latitude: row.lat(),
+            longitude: row.lng()
+        })
+    }
+
+    register.servingArea.coordinates = coords;
 }
 
 function autoComplete() {
@@ -94,6 +214,18 @@ function autoComplete() {
             title: place.name,
             position: place.geometry.location,
             draggable: true,
+        });
+
+        register.deparmentLocation.coordinates = {
+            latitude: marker.getPosition().lat(),
+            longitude: marker.getPosition().lng()
+        };
+
+        marker.addListener('dragend', function (event) {
+            register.deparmentLocation.coordinates = {
+                latitude: event.latLng.lat(),
+                longitude: event.latLng.lng()
+            }
         });
 
 
